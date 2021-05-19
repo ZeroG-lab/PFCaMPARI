c8d2cea4647dc3a12a6b8e8d3c5f4740485c56b9
HammerAndreas <hammer.andreas@hotmail.com>
2021-05-19 16:56:41 +0200

    Try to summarize first event

--- parserPFCaMPARI.R
+++ parserPFCaMPARI.R
@@ -48,3 +48,21 @@ PFCaMPARI$col <- gsub('^.', '', PFCaMPARI$Well)
 PFCaMPARI$row <- substring(PFCaMPARI$Well,1 ,1)
 col_order <- c("row","col","Well","ConversionRate","Flight","Unit", "Plate", "Treatment")
 PFCaMPARI <- PFCaMPARI[, col_order]
+
+
+# Import Hardware flight data: 50 sets per second,
+# 8 second for every event -> 400 entries per illumination
+
+# Step One: Delete Entries till the first event
+
+Unit101_Flight1 <- read.csv2("./Unit101_Flight1.csv", header=TRUE)
+
+#extract column information from first trigger to first stop
+
+event1 <- Unit101_Flight1$event[c(1:200),]
+
+event1 <- Unit101_Flight1$event[c(".*LED ON*":".*LEDs off"),]
+
+event1 <- Unit101_Flight1[c("Board: 1 LED ON H9, Board: 1 LED ON H3, Board: 2 LED ON H9, Board: 2 LED ON H3":"LEDs off, Board: 1 LED ON H12, Board: 1 LED ON H6, Board: 2 LED ON H12, Board: 2 LED ON H6
+"),]
+
