6da98489b797505f1b0acac55523820dd4afb69d
HammerAndreas <hammer.andreas@hotmail.com>
2021-05-28 13:18:25 +0200

    Added G-Phases Changed well-order adjusted data-types

--- parserPFCaMPARI.R
+++ parserPFCaMPARI.R
@@ -1,12 +1,14 @@
 #Load libraries
 library(readxl)
 
+#####READ INCUCYTE DATA#################################################################################
+
 #prepare empty data frame for loop binding
 PFCaMPARI <- data.frame()
 
 #loop through all 3 flights
 for (k in 1:3) {
-#loop through all sheets in each flight data set
+  #loop through all sheets in each flight data set
   for (i in 1:8) {
     #read data
     df_Flight <- read_xlsx(paste0("./Flight_", k, ".xlsx"), sheet = i)
@@ -29,8 +31,8 @@ for (k in 1:3) {
     #create metadata columns and add respective metadata entries
     Flight$Flight <- gsub("^.*t", "", metadata[1])
     Flight$Unit <- gsub("^.*t", "", metadata [2])
-    Flight$Plate <- gsub("^.*e", "", metadata [3])
-    Flight$Treatment <-metadata [4]
+    Flight$Board <- gsub("^.*e", "", metadata [3])
+    Flight$Inhibitor <-metadata [4]
     
     #rename first two unnamed columns
     colnames(Flight)[1:2] <- c("Well", "ConversionRate")
@@ -48,83 +50,132 @@ for (k in 1:3) {
 # added extra columns in the dataframe for 96 well plate row and column
 # to make it compatible with the Bioassays package
 
-PFCaMPARI$col <- gsub('^.', '', PFCaMPARI$Well)
-PFCaMPARI$row <- substring(PFCaMPARI$Well,1 ,1)
-col_order <- c("row","col","Well","ConversionRate","Flight","Unit", "Plate", "Treatment")
-PFCaMPARI <- PFCaMPARI[, col_order]
+# PFCaMPARI$col <- gsub('^.', '', PFCaMPARI$Well)
+# PFCaMPARI$row <- substring(PFCaMPARI$Well,1 ,1)
+# col_order <- c("row","col","Well","ConversionRate","Flight","Unit", "Board", "Treatment")
+# PFCaMPARI <- PFCaMPARI[, col_order]
 
+
+
+
+
+#####READ IN HARDWARE DATA################################################################################
 
 #prepare empty data frame for loop binding
 PFC_Hardware <-data.frame()
 
-for(k in list.files(path="./", pattern = "\\.csv", full.names = TRUE)){
+for(k in list.files(path="./", pattern = "^Unit.*\\.csv", full.names = TRUE)){
   print(paste("working on",k))
-
-Flight <- read.csv(k, sep=";", header=TRUE)
-
-#prepare empty data frame for loop binding
-Flight_AVG<-data.frame()
- 
-#loop through all rows, that have an LED ON event 
-for (i in grep("LED ON",Flight$event)) {
   
-  #average the measurements of the 8 second sequence
-  colmean<-data.frame(t(colMeans(Flight[i:(i+399),-c(1,11,12)])))
- 
-  #construct string with metadata information about well and board position
-  metastring <- unique(gsub("Board: ", "", unlist(strsplit(unlist(strsplit(gsub("LEDs off, ", "", Flight$event[i]), split = ", ")), split = " LED ON "))))
- 
-  #add metadata to the averaged measurements
-  colmean$Board <- paste(metastring[1])
-  colmean$Well <- paste(metastring[2])
- 
-  #prepare time string for correction by splitting it up
-  timestring <- unlist(strsplit(Flight$time[i], split = ":"))
- 
-  #if there is a single-digit minute value, add a zero. If not, ignore.
-  if(nchar(timestring[2]) == 1) {
-   timestring[2] <- paste0(0, timestring[2])
-  }else{}
- 
-  #collapse the split and corrected time string back into the time column
-  colmean$Time <-  paste(timestring, collapse = ":")
-  #convert string to time
-  Flight$time <- as.POSIXct(Flight$time,format="%H:%M:%OS")
-  #Add column "Unit" with 101/102/103/104  to Flight_AVG dataframe
-  d1 <- read.table(text=gsub("^.*it|Flight|.csv", "", k),
-                   sep = "_", col.names = c("Unit" , "Flight"))
+  Flight <- read.csv(k, sep=";", header=TRUE)
   
-  colmean <- cbind(colmean, d1)
+  #prepare empty data frame for loop binding
+  Flight_AVG<-data.frame()
   
-  #add a new row of duplicated data for each board and well and paste the corresponding metadata to it
-  colmean <- rbind(colmean,colmean[1,])
-  colmean$Board[2] <- paste(metastring[1])
-  colmean$Well[2] <- paste(metastring[3])
- 
-  colmean <- rbind(colmean,colmean[1,])
-  colmean$Board[3] <- paste(metastring[4])
-  colmean$Well[3] <- paste(metastring[2])
- 
-  colmean <- rbind(colmean,colmean[1,])
-  colmean$Board[4] <- paste(metastring[4])
-  colmean$Well[4] <- paste(metastring[3])
- 
-  #produce human-readable column names for the finished data set
-  colnames(colmean)[1:9] <- c("Temp.Board.1", "Temp.Board.2", "Temp.LED", "Pressure.mbar", "Gravity.X.mg", "Gravity.Y.mg", "Gravity.Z.mg", "Supply.U.mV", "Supply.I.mA")
- 
-  #bind the data from the previous loop with data from this loop to generate the final data frame
-  Flight_AVG<-rbind(Flight_AVG,colmean)
+  #loop through all rows, that have an LED ON event 
+  for (i in grep("LED ON",Flight$event)) {
+    
+    #average the measurements of the 8 second sequence
+    colmean<-data.frame(t(colMeans(Flight[i:(i+399),-c(1,11,12)])))
+    
+    #construct string with metadata information about well and board position
+    metastring <- unique(gsub("Board: ", "", unlist(strsplit(unlist(strsplit(gsub("LEDs off, ", "", Flight$event[i]), split = ", ")), split = " LED ON "))))
+    
+    #add metadata to the averaged measurements
+    colmean$Board <- paste(metastring[1])
+    colmean$Well <- paste(metastring[2])
+    
+    
+    #convert string to time
+    colmean$Time <- as.POSIXct(Flight$time[i],format="%H:%M:%OS")
+    
+    #Add column "Unit" with 101/102/103/104  to Flight_AVG dataframe
+    d1 <- read.table(text=gsub("^.*it|Flight|.csv", "", k),
+                     sep = "_", col.names = c("Unit" , "Flight"))
+    
+    colmean <- cbind(colmean, d1)
+    
+    #add a new row of duplicated data for each board and well and paste the corresponding metadata to it
+    colmean <- rbind(colmean,colmean[1,])
+    colmean$Board[2] <- paste(metastring[1])
+    colmean$Well[2] <- paste(metastring[3])
+    
+    colmean <- rbind(colmean,colmean[1,])
+    colmean$Board[3] <- paste(metastring[4])
+    colmean$Well[3] <- paste(metastring[2])
+    
+    colmean <- rbind(colmean,colmean[1,])
+    colmean$Board[4] <- paste(metastring[4])
+    colmean$Well[4] <- paste(metastring[3])
+    
+    #produce human-readable column names for the finished data set
+    colnames(colmean)[1:9] <- c("Temp.Board.1", "Temp.Board.2", "Temp.LED", "Pressure.mbar", "Gravity.X.mg", "Gravity.Y.mg", "Gravity.Z.mg", "Supply.U.mV", "Supply.I.mA")
+    
+    #bind the data from the previous loop with data from this loop to generate the final data frame
+    Flight_AVG<-rbind(Flight_AVG,colmean)
+    
+    #correct column names if necessary
+    colnames(Flight_AVG)<-colnames(colmean)
+    
+    
+    #clean up
+    rm(colmean, metastring, d1,i)
+    
+  }
+  #bind all data from each hardware unit and flight
+  PFC_Hardware <- rbind(PFC_Hardware,Flight_AVG)
   
-  #correct column names if necessary
-  colnames(Flight_AVG)<-colnames(colmean)
-  
-  
-  #clean up
-  rm(colmean, metastring, timestring,i)
-
 }
 
-PFC_Hardware <- rbind(PFC_Hardware,Flight_AVG)
+#Produce metadata table for assigning parabola, construct, and led trigger number to main data
+platemap <- data.frame("Well"= unique(PFCaMPARI$Well),
+                       "Parabola" = rep(c(1,2,3,4),each=24),
+                       "Construct" = rep(c("CaMPARI2","CaMPARI2-F391W"), each=6),
+                       "LED.trigger" = c(1,2),
+                       "Condition" = ""
+)
 
-}
 
+#Assign G-Phase conditions to platemap
+platemap$Construct[grep("A2|A8|C2|C8|E2|E8|G2|G8", platemap$Well)] <- "GFP"
+platemap$LED.trigger[grep("[ACEG][1278]$", platemap$Well)] <- NA
+platemap$Condition[grep("[ACEG][17]$", platemap$Well)] <- "Histamine"
+platemap$Condition[grep("[ACEG](10|[23489])", platemap$Well)] <- "Pre-Parabola"
+platemap$Condition[grep("[ACEG](10|[23489])", platemap$Well)] <- "Pre-Parabola"
+platemap$Condition[grep("[ACEG](5|6|11|12)", platemap$Well)] <- "Pull-up"
+platemap$Condition[grep("[BDFH][1278]$", platemap$Well)] <- "Zero-G"
+platemap$Condition[grep("[BDFH](10|[349])", platemap$Well)] <- "Pull-out"
+platemap$Condition[grep("[BDFH](5|6|11|12)", platemap$Well)] <- "Post-Parabola"
+
+
+
+
+#####MERGE INCUCYTE DATA WITH HARDWARE DATA#########################################################
+
+
+#Merge data frames and keep all entries
+PFC_Merged <- merge(PFCaMPARI, PFC_Hardware, all = TRUE)
+
+PFC_Merged <- merge(PFC_Merged, platemap, all = TRUE)
+
+#Correct Ruthenium Red/ GSK2193874 treatment
+PFC_Merged$Inhibitor <- gsub("RuthRed|RutheniumRed", "Ruthenium Red", PFC_Merged$Inhibitor)
+PFC_Merged$Inhibitor <- gsub("GSK219", "GSK2193874", PFC_Merged$Inhibitor)
+
+#Rename Inhibitor treatments
+PFC_Merged$Inhibitor <- gsub("untreated", "None" ,PFC_Merged$Inhibitor)
+
+#OPTIONAL: Add identifier columns for BioAssays package
+PFC_Merged$col <- gsub('^.', '', PFC_Merged$Well)
+PFC_Merged$row <- substring(PFC_Merged$Well,1 ,1)
+PFC_Merged <- PFC_Merged[, c(22,21,1:4,6,17:20,5,7:16)]
+
+#Order wells, change data-types
+PFC_Merged$col <- as.numeric(PFC_Merged$col)
+PFC_Merged$Well <- factor(PFC_Merged$Well, levels=platemap$Well, ordered=TRUE)
+PFC_Merged$Parabola <- as.character(PFC_Merged$Parabola)
+PFC_Merged$LED.trigger <- as.character(PFC_Merged$LED.trigger)
+
+
+#Write out data
+write.csv(PFC_Merged, "./PFC_Merged.csv", quote = FALSE, row.names = FALSE)
